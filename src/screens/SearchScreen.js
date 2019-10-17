a1057739e33e1b33e4087a55ba82bc7e3606ad17
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-17 16:40:50 +0100

    Initial Commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
