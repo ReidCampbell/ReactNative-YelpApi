8de494f2c3f4d59b946bf73a797fb68a5528bfcd
ReidCampbell <reidscottcampbell@gmail.com>
2019-10-19 15:20:44 +0100

    created resultshow screen

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const ResultsShowScreen = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
